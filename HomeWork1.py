dc573a428a39adf18a6d8a286514876d41248579
Volodymyr-SV <volmihsv2gmail.com>
2019-01-30 16:59:25 +0200

    Task1

---
+++ HomeWork1.py
@@ -0,0 +1,4 @@
+name = input("what is your name?\t")
+age = input("HOw old are you?\t")
+livePlace = input("Where do you live(city)?\t")
+print("Hello, "+name+"\tYour age is "+age+"\tYou live in "+livePlace)
