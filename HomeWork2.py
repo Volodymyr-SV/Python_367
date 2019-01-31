053d2023fe1079d8632d8b749aec87540074a1af
Volodymyr-SV <volmihsv2gmail.com>
2019-01-31 14:57:27 +0200

    task2

---
+++ HomeWork2.py
@@ -0,0 +1,12 @@
+a=int(input("a="))
+b=int(input("b="))
+plus=a+b
+minus=b-a
+multiply=a*b
+divide=a/b
+divide_integer=a//b
+vestige=a%b
+print(" a+b=", a+b, "\n b-a=",minus, "\n a*b=",multiply, "\n a/b=", divide, "\n a//b=", divide_integer, "\n a%b= ",vestige)
+
+
+#print("a+b="+)
