05357ee7380f08b29dd72239c82c3d0d3316edcb
Volodymyr-SV <volmihsv2gmail.com>
2019-01-31 17:19:33 +0200

    class work 1

--- ClassWork1.py
+++ ClassWork1.py
@@ -5,3 +5,18 @@ if a<b:
 elif a>b:
     print(a,">",b,", a bilshe")
 else: print(a,"=",b,", rivni")
+
+#######################
+
+c=int(input("c="))
+if c % 2 == 0 :
+    print("parne")
+else: print("neparne")
+
+########################   
+
+d=int(input("d=")) 
+f=1
+for i in range(1,d+1):
+    f=f*i
+print("factorial=",f)
